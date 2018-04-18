e668293539189126728420517501ae25000916f8
Muheb.moshfiq <mohibkhan@hotmail.com>
2018-04-18 11:37:00 +0200

    Sell activity updated

--- app/src/main/java/com/example/gvida/ticketapp2/SellActivity.java
+++ app/src/main/java/com/example/gvida/ticketapp2/SellActivity.java
@@ -1,9 +1,18 @@
 package com.example.gvida.ticketapp2;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Rehan Mir on 17-04-2018.
  */
 
-public class SellActivity {
+public class SellActivity extends AppCompatActivity{
 
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sell_activity);
+
+    }}
